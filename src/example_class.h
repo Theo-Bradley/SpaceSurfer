6eb8abd1cfd138aea58707d207dbfcf3eacef3d8
Theo Bradley <gaminggekoman@gmail.com>
2025-05-29 16:56:08 +0100

    Renamed project and extension Renamed demo to surfer Renamed example.gdextension to SpaceSurfer.gdextension changed SConstruct and .gdextension to reflect these changes

--- src/example_class.h
+++ src/example_class.h
@@ -3,11 +3,12 @@
 #include "godot_cpp/classes/ref_counted.hpp"
 #include "godot_cpp/classes/wrapped.hpp"
 #include "godot_cpp/variant/variant.hpp"
+#include "godot_cpp/classes/node3d.hpp"
 
 using namespace godot;
 
-class ExampleClass : public RefCounted {
-	GDCLASS(ExampleClass, RefCounted)
+class ExampleClass : public Node3D {
+	GDCLASS(ExampleClass, Node3D)
 
 protected:
 	static void _bind_methods();
