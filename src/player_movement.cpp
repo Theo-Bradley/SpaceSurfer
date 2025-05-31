02935e3911a3f48c1fe52266f6c10d46ababde17
Theo Bradley <gaminggekoman@gmail.com>
2025-05-31 15:07:18 +0100

    Added side to side movement prototype

--- src/player_movement.cpp
+++ src/player_movement.cpp
@@ -1,6 +1,8 @@
 #include "player_movement.h"
 #include <godot_cpp/core/class_db.hpp>
 
+#define input Input::get_singleton()
+
 using namespace godot;
 
 void PlayerMovement::_bind_methods()
@@ -12,15 +14,37 @@ void PlayerMovement::_bind_methods()
 	//add the property by setting the getters and setters function names to the ones we added above
 	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "Player Rigid Body", PROPERTY_HINT_NODE_TYPE, "RigidBody3D"), "set_playerRigidBody", "get_playerRigidBody");
 
-	ClassDB::bind_method(D_METHOD("get_jumpForce"), &PlayerMovement::get_jumpForce); //add getters and setters to classdb
+	//..
+	ClassDB::bind_method(D_METHOD("get_jumpForce"), &PlayerMovement::get_jumpForce);
 	ClassDB::bind_method(D_METHOD("set_jumpForce", "force"), &PlayerMovement::set_jumpForce);
 
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "Jump Impulse"), "set_jumpForce", "get_jumpForce");
+
+	ClassDB::bind_method(D_METHOD("get_moveSpeed"), &PlayerMovement::get_moveSpeed);
+	ClassDB::bind_method(D_METHOD("set_moveSpeed", "speed"), &PlayerMovement::set_moveSpeed);
+
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "Move Speed"), "set_moveSpeed", "get_moveSpeed");
+
+	ClassDB::bind_method(D_METHOD("get_laneWidth"), &PlayerMovement::get_laneWidth);
+	ClassDB::bind_method(D_METHOD("set_laneWidth", "width"), &PlayerMovement::set_laneWidth);
+
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "Lane Width"), "set_laneWidth", "get_laneWidth");
+	
+	ClassDB::bind_method(D_METHOD("get_easingStart"), &PlayerMovement::get_easingStart);
+	ClassDB::bind_method(D_METHOD("set_easingStart", "start"), &PlayerMovement::set_easingStart);
+
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "Easing Start"), "set_easingStart", "get_easingStart");
+
+	ClassDB::bind_method(D_METHOD("get_movementStop"), &PlayerMovement::get_movementStop);
+	ClassDB::bind_method(D_METHOD("set_movementStop", "stop"), &PlayerMovement::set_movementStop);
+
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "Movement Stop"), "set_movementStop", "get_movementStop");
 }
 
 PlayerMovement::PlayerMovement()
 {
 	// Initialize any variables here.
+	moveDirection = None;
 }
 
 PlayerMovement::~PlayerMovement()
@@ -30,11 +54,34 @@ PlayerMovement::~PlayerMovement()
 
 void PlayerMovement::_process(double delta)
 {
-	if (Input::get_singleton()->is_action_just_pressed("Jump"))
+	if (input->is_action_just_pressed("Jump"))
 	{
 		print_line("jumping");
-		playerRigidBody->apply_central_impulse(Vector3(0.f, jumpForce, 0.f));
+		playerRigidBody->apply_central_impulse(Vector3(0.f, jumpForce * playerRigidBody->get_mass(), 0.f));
 	}
+	if (input->is_action_just_pressed("Left"))
+	{
+		moveDirection = Left;
+	}
+	if (input->is_action_just_pressed("Right"))
+	{
+		moveDirection = Right;
+	}
+}
+
+void PlayerMovement::_physics_process(double delta)
+{
+	int change = moveDirection == Left ? -1 : 1;
+	if (moveDirection == None)
+		change = 0;
+	int lane = moveDirection == Left ? 0 : 1;
+	float dist = Math::absf(lane * laneWidth - playerRigidBody->get_global_position().x);
+	float easing = fminf(powf(dist * powf(easingStart, -1.f), 0.5f), 1.0f); //y = min((x*a)^(1/2)) where x is distance between player and lane and 1/a is the start of easing
+	float v = moveSpeed * easing * change;
+	float u = playerRigidBody->get_linear_velocity().x;
+	playerRigidBody->apply_central_impulse(Vector3((v - u) * playerRigidBody->get_mass(), 0.f, 0.f));
+	if (easing <= movementStop)
+		moveDirection = None;
 }
 
 //getters and setters for ClassDB
@@ -57,4 +104,42 @@ void PlayerMovement::set_jumpForce(float force)
 float PlayerMovement::get_jumpForce() const
 {
 	return jumpForce;
+}
+
+void PlayerMovement::set_moveSpeed(float speed)
+{
+	moveSpeed = speed;
+}
+
+float PlayerMovement::get_moveSpeed() const
+{
+	return moveSpeed;
+}
+
+void PlayerMovement::set_laneWidth(float width)
+{
+	laneWidth = width;
+}
+float PlayerMovement::get_laneWidth() const
+{
+	return laneWidth;
+}
+
+void PlayerMovement::set_easingStart(float start)
+{
+	easingStart = start;
+}
+float PlayerMovement::get_easingStart() const
+{
+	return easingStart;
+}
+
+void PlayerMovement::set_movementStop(float stop)
+{
+	movementStop = stop;
+}
+
+float PlayerMovement::get_movementStop() const
+{
+	return movementStop;
 }
--- src/player_movement.h
+++ src/player_movement.h
@@ -3,6 +3,8 @@
 #include "godot_cpp/classes/node3d.hpp"
 #include "godot_cpp/classes/input.hpp"
 #include "godot_cpp/classes/rigid_body3d.hpp"
+#include "godot_cpp/classes/physics_direct_body_state3d.hpp"
+///#include "godot_cpp/core/math.hpp"
 
 using namespace godot;
 
@@ -11,6 +13,15 @@ class PlayerMovement : public Node3D {
 
 protected:
 	static void _bind_methods();
+
+	enum MovementDirection
+	{
+		None,
+		Left,
+		Right
+	};
+
+	MovementDirection moveDirection;
 
 public:
 	PlayerMovement();
@@ -24,5 +35,22 @@ public:
 	void set_jumpForce(float force);
 	float get_jumpForce() const;
 
+	float moveSpeed = 0.0f;
+	void set_moveSpeed(float speed);
+	float get_moveSpeed() const;
+
+	float laneWidth = 0.0f;
+	void set_laneWidth(float width);
+	float get_laneWidth() const;
+
+	float easingStart = 0.0f;
+	void set_easingStart(float start);
+	float get_easingStart() const;
+
+	float movementStop = 0.0f;
+	void set_movementStop(float stop);
+	float get_movementStop() const;
+
 	void _process(double delta) override;
+	void _physics_process(double delta) override;
 };
